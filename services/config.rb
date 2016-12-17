7e5f4a1bbe0b729e97990295e616cd547a2fad47
emilkip <emil@cloudcoreo.com>
2016-12-17 10:05:20 +0600

    fixes

--- services/config.rb
+++ services/config.rb
@@ -12,26 +12,11 @@ coreo_aws_advisor_alert "get-security-groups" do
   alert_when [//]
 end
 
-coreo_aws_advisor_alert "get-active-security-groups" do
-  action :define
-  service :ec2
-  display_name "List of active security groups"
-  description "Gets all active security groups"
-  category "Inventory"
-  suggested_action "None."
-  level "Informational"
-  objectives ["spot_instance_requests"]
-  audit_objects ["spot_instance_request_set.network_interface_set.security_group_id"]
-  operators ["=~"]
-  alert_when [//]
-end
-
 coreo_aws_advisor_ec2 "advise-ec2" do
   action :advise
   alerts ["get-security-groups", "get-active-security-groups"]
   regions ${AUDIT_AWS_ELB_REGIONS}
 end
-
 
 coreo_aws_advisor_alert "get-active-security-groups-from-elb" do
   action :define
@@ -42,7 +27,7 @@ coreo_aws_advisor_alert "get-active-security-groups-from-elb" do
   suggested_action "None."
   level "Informational"
   objectives ["load_balancers"]
-  audit_objects ["load_balancers.load_balancer_descriptions.security_groups"]
+  audit_objects ["load_balancer_descriptions.security_groups"]
   operators ["=~"]
   alert_when [//]
 end
